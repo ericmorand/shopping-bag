3cc6237e822544da46b1ab4a23c6923a3691fb77
Eric <ericmorand@iMac-de-Celine.local>
2014-01-11 13:40:04 +0100

    Add Tax column to Product module

--- SalesModule.m
+++ SalesModule.m
@@ -864,7 +864,7 @@
 			NSDate *startDate = [NSCalendarDate dateWithYear:year month:1 day:1 hour:0 minute:0 second:0 timeZone:[NSTimeZone defaultTimeZone]];
 			NSDate *endDate = [NSCalendarDate dateWithYear:(year + 1) month:1 day:1 hour:0 minute:0 second:0 timeZone:[NSTimeZone defaultTimeZone]];
 			
-			NSLog(@"startDate = %@ -> endDate = %@", startDate, endDate);
+			// NSLog(@"startDate = %@ -> endDate = %@", startDate, endDate);
 			
 			return [NSArray arrayWithObjects:startDate, endDate, nil];
 		}
